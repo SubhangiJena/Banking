6e41722bfc110f6bcd70fc79cbb0c061c2626feb
Subhangi-J <subhangi.jena@lntinfotech.com>
2021-07-29 19:37:52 +0530

    Commited

---
+++ HDFC.java
@@ -0,0 +1,8 @@
+
+class HDFC{
+public static void main(String args[]){
+System.out.println("HDFC git");
+
+
+}
+}
---
+++ ICIC.java
@@ -0,0 +1,8 @@
+
+class ICIC{
+public static void main(String args[]){
+System.out.println("ICIC git");
+
+
+}
+}
